a76821f7452c15c484a4223dd41d545fdb07434e
Anshuman <chaoanxuman999@gmail.com>
2025-08-14 00:55:59 +0530

    User Service done

---
+++ userService/src/main/java/com/fitness/userService/Controller/UserController.java
@@ -0,0 +1,30 @@
+package com.fitness.userService.Controller;
+
+
+import com.fitness.userService.DTO.RegisterRequest;
+import com.fitness.userService.DTO.UserResponse;
+import com.fitness.userService.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor //wont need autowired
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+
+}
---
+++ userService/src/main/java/com/fitness/userService/service/UserService.java
@@ -0,0 +1,51 @@
+package com.fitness.userService.service;
+
+
+import com.fitness.userService.DTO.RegisterRequest;
+import com.fitness.userService.DTO.UserResponse;
+import com.fitness.userService.model.User;
+import com.fitness.userService.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserResponse register(@Valid RegisterRequest request) {
+
+        if(userRepository.existsByEmail(request.getEmail())){
+            throw new RuntimeException("E-mail already exists");
+        }
+        User user=new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+       User savedUser= userRepository.save(user);
+       UserResponse userResponse=new UserResponse();
+       userResponse.setId(user.getId());
+       userResponse.setPassword(user.getPassword());
+       userResponse.setEmail(user.getEmail());
+       userResponse.setFirstName(user.getFirstName());
+       userResponse.setLastName(user.getLastName());
+       userResponse.setCreatedAt(user.getCreatedAt());
+       userResponse.setUpdatedAt(user.getUpdatedAt());
+       return userResponse;
+    }
+
+    public UserResponse getUserProfile(String userId) {
+        User user=userRepository.findById(userId).orElseThrow(()->new RuntimeException("User not found"));
+        UserResponse userResponse=new UserResponse();
+        userResponse.setId(user.getId());
+        userResponse.setPassword(user.getPassword());
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setCreatedAt(user.getCreatedAt());
+        userResponse.setUpdatedAt(user.getUpdatedAt());
+        return userResponse;
+    }
+}
